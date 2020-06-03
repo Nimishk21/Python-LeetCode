ba912654361c4f84f397c0b8a4b05ac083462f60
Nimish Kanani <nimishk21@gmail.com>
2020-06-03 11:24:36 -0500

    Update Remove element.py

--- Remove element.py
+++ Remove element.py
@@ -7,3 +7,11 @@ class Solution:
             else:
                 i=i+1
         return len(nums)
+    
+#method 2:
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
+        
