741f107e54ebe881e5aa41c894470308508696e3
Nimish Kanani <nimishk21@gmail.com>
2020-06-07 12:57:19 -0500

    Update Longest Common Prefix.py

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -28,3 +28,18 @@ class Solution:
             else:
                 break
         return p
+    #method 3:
+    class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        if len(strs)==1:
+            return strs[0]
+        p=0
+        strs.sort()
+        for x,y in zip(strs[0],strs[-1]):
+            if x==y:
+                p+=1
+            else:
+                break
+        return strs[0][:p]
