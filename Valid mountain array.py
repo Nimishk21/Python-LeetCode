105a2b811d79c81aca63a88456fe9934b12ad132
Nimish Kanani <nimishk21@gmail.com>
2020-06-16 17:52:45 -0500

    Update Valid mountain array.py

--- Valid mountain array.py
+++ Valid mountain array.py
@@ -2,7 +2,8 @@ class Solution(object):
     def validMountainArray(self, A):
         N = len(A)
         i = 0
-
+        if N<3 or A[0]>=A[1]:
+            return False
         # walk up
         while i+1 < N and A[i] < A[i+1]:
             i += 1
@@ -16,3 +17,21 @@ class Solution(object):
             i += 1
 
         return i == N-1
+    
+class Solution:
+    def validMountainArray(self, A: List[int]) -> bool:
+        
+        if len(A)<3 or A[0]>=A[1]:
+            return False
+			
+        uphill = True
+        
+        for i in range(1,len(A)):
+            if uphill:
+                if A[i-1]>=A[i]:
+                    uphill = False
+            if not uphill:
+                if A[i-1]<=A[i]:
+                    return False
+        return not uphill    
+    
