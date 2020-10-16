1f6782f440206d9e47f44c10c0755487b1e5d182
Nimish Kanani <nimishk21@gmail.com>
2020-10-15 23:59:52 -0500

    Update Valid Palindrome.py

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -2,3 +2,16 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = re.sub('[^a-z0-9]', '', str(s).lower())
         return s[::-1] == s
+    
+#method 2:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        str1=""
+        for i in s:
+            if i.isalnum()==True:
+                str1+=i
+        str2=str1[::-1]
+        if str1.lower()==str2.lower():
+            return True
+        else:
+            return False
